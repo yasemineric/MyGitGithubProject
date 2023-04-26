dd7f43d1530eda0ea09b4e1a545d884f39b3e302
Yasemin Eric <yasemineric6@gmail.com>
2023-04-26 12:41:54 +0200

    ysm3

--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,9 @@
 public class Account {
     public static void main(String[] args) {
         System.out.println("");
-        for (int i = 0; i < 10; i++) {
+        System.out.println("yeni");
 
-        }
+
 
 
     }
