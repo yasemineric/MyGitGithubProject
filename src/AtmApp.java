540037b70827d681ecbcef081737c868938fe6e4
Yasemin Eric <yasemineric6@gmail.com>
2023-04-26 11:02:32 +0200

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -2,6 +2,8 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir.");
         //version1
+        System.out.println("Java is easy");
+        //version2
 
 
 
